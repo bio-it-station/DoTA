fix ddof=0 in std calculation

test_and_debug_scripts/dzpsi2.py
@@ -27,7 +27,7 @@ with open('/home/chenghung/DoTA/input/rf_data/human_weight_rf_data_filtered.pick
 # Create psi table
 psi_df = y.pivot(index='Gene', columns='Tissue')['PSI']
 # Calculate stdev
-psi_stdev = psi_df.std(axis=1, ddof=0)
+psi_stdev = psi_df.std(axis=1)
 # Drop genes with no stdev across different tissues
 keep_idx = psi_stdev[psi_stdev != 0].index
 psi_df = psi_df.loc[keep_idx]

utils.py
@@ -62,7 +62,7 @@ def filter_less_than_3(x, y):
     psi_df = y.pivot(index='Gene', columns='Tissue')['PSI']
 
     # Calculate stdev
-    psi_stdev = psi_df.std(axis=1, ddof=0)
+    psi_stdev = psi_df.std(axis=1)
 
     # Drop genes with no stdev across different tissues
     keep_gene = psi_stdev[psi_stdev != 0].index